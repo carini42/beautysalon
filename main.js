9ca6e5ab6bc58b546da5f31c1970def5a4df6fcb
Fernanda Carini <carinidev@gmail.com>
2021-06-24 23:05:36 -0300

    Criação layout, menu mobile

---
+++ main.js
@@ -0,0 +1,33 @@
+/*abre e fecha o menu quando clicar no icone: hamburguer e x*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*quando clicar em um item do menu, esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*mudar o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //scroll é menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
