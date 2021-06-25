bd069ee4eccb7d12c56f98c33fd0ec1874fa5ef1
Fernanda Carini <carinidev@gmail.com>
2021-06-25 20:22:01 -0300

    Construção de novos elementos, fazendo ficar dinâmico e único, mais programação Javascript e ajustes CSS, uso de códigos de outros desenvolvedores.

--- main.js
+++ main.js
@@ -31,3 +31,37 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* testimonials carouser slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  sliderPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* ScrollReveal: mostrar elementos quando der scroll na página*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+)
