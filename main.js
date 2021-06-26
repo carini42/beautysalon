d683665bc5a299d650417d8f04c30442f5700756
Fernanda Carini <carinidev@gmail.com>
2021-06-26 09:56:02 -0300

    Ajuste final botão scroll to top

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ for (const link of links) {
 }
 
 /*mudar o header da página quando der scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -30,8 +30,7 @@ window.addEventListener('scroll', function () {
     //scroll é menor que a altura do header
     header.classList.remove('scroll')
   }
-})
-
+}
 /* testimonials carouser slider swiper */
 const swiper = new Swiper('.swiper-container', {
   sliderPerView: 1,
@@ -61,9 +60,26 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
+
 `,
   { interval: 100 }
 )
 
 /*botão voltar para o topo*/
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*when scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
